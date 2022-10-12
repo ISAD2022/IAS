d0ef77cc4764ffc1eaadf0d969df790bf9e55968
Ali Asif <ali_asif35@hotmail.com>
2022-10-12 16:32:49 +0500

    latest session

--- AIS/AIS/wwwroot/js/site.js
+++ AIS/AIS/wwwroot/js/site.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   
 
         const idleDurationSecs = 300;    // X number of seconds
-        const redirectUrl = './Login/logout';  // Redirect idle users to this URL
+        const redirectUrl = "/Login/logout";  // Redirect idle users to this URL
         let idleTimeout; // variable to hold the timeout, do not modify
 
         const resetIdleTimeout = function () {
