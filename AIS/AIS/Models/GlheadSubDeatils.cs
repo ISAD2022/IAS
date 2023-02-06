6a5b7c9dc901f2bdeff13c19153fbc998b95b276
Ali Asif <ali_asif35@hotmail.com>
2023-02-06 11:39:13 +0500

    Revert "Code After Merge Asfand"
    This reverts commit 35c5d18a4c59b0618e56f6cf1e77640982448c87.

--- AIS/AIS/Models/GlheadSubDeatils.cs
+++ AIS/AIS/Models/GlheadSubDeatils.cs
@@ -7,18 +7,15 @@ namespace AIS.Models
     public class GlHeadSubDetailsModel
     {        
         public int BRANCHID { get; set; }
-        public int GLSUBCODE { get; set; }
-
-    
-    
+        public int GL_TYPE_ID { get; set; }
+        public int GLSUBID{ get; set; }
+        public int GLCODE{ get; set; }
         public string GLSUBNAME { get; set; }
-        public string DESCRIPTION { get; set; }
-
-
-       
+        
+        public DateTime DATETIME { get; set; }
         public double BALANCE { get; set; }
-        public double DEBIT { get; set; }
-        public double CREDIT { get; set; }
+        public double RUNNING_DR { get; set; }
+        public double RUNNING_CR { get; set; }
         public List<GlHeadSubDetailsModel> GL_SUBDETAILS { get; set; }
 
     }
