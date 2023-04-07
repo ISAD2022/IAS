566503156e0f3f79bdb0db42d82efcab6c699ac6
Ali Asif <ali_asif35@hotmail.com>
2023-04-07 12:53:03 +0500

    Changes on AIS latest System

--- AIS/AIS/DBConnection.cs
+++ AIS/AIS/DBConnection.cs
@@ -49,8 +49,8 @@ namespace AIS
             {
                 OracleConnection con = new OracleConnection();
                 OracleConnectionStringBuilder ocsb = new OracleConnectionStringBuilder();
-                ocsb.Password = "ztblaisdev";
-                ocsb.UserID = "ztblaisdev";
+                ocsb.Password = "ztblais";
+                ocsb.UserID = "ztblais";
                 ocsb.DataSource = "10.1.100.222:1521/devdb18c.ztbl.com.pk";
                 // connect
                 con.ConnectionString = ocsb.ConnectionString;
