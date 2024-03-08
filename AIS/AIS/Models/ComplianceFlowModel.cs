b06306e3a491c10791cb46ef0847baf68251fe6c
Mohsin Shah <engrmohsin00@gmail.com>
2024-03-07 22:36:37 -0800

    Update ComplianceFlowModel.cs

--- AIS/AIS/Models/ComplianceFlowModel.cs
+++ AIS/AIS/Models/ComplianceFlowModel.cs
@@ -4,23 +4,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AIS.Models
 {
-    public class ComplianceHistoryModel
+    public class ComplianceFlowModel
     {
         public int ID { get; set; }
-        public string REF_P { get; set; }
-        public string OBS_ID { get; set; }
-        public string REMARKS { get; set; }
-        public string ATTENDED_ON { get; set; }
-        public string ATTENDED_BY { get; set; }
-        public string ROLE_ID { get; set; }
-        public string STAGE { get; set; }
-        public string NAME { get; set; }
-        public string DESIGNATION { get; set; }
-        public string AUDITED_BY { get; set; }
-        public string ENTITY_ID { get; set; }
-        public string C_STATUS { get; set; }
-        public string SEQ { get; set; }
-        public string COM_SEQ_NO { get; set; }
-        public string PARA_CATEGORY { get; set; }
+        public string ENTITY_TYPE_ID { get; set; }
+        public string GROUP_ID { get; set; }
+        public string NEXT_GROUP_ID { get; set; }
+        public string PREV_GROUP_ID { get; set; }
+       
     }
 }
