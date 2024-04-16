94b4b5e471c40d18b03e0aba05b4243b4a9173dc
Ali Asif <ali_asif35@hotmail.com>
2024-04-16 12:10:59 +0500

    Model Model GLHEAD

--- AIS/AIS/Models/Glheadsummaryyearly.cs
+++ AIS/AIS/Models/Glheadsummaryyearly.cs
@@ -6,24 +6,25 @@ namespace AIS.Models
 {
     public class Glheadsummaryyearlymodel
     {
+
+        public string COL1 { get; set; }
+        public string COL2 { get; set; }
+        public string COL3 { get; set; }
         public int GLSUBCODE { get; set; }
         public int BRANCHID { get; set; }
-        public string GLSUBNAME { get; set; }
-    
-        
-     
+        public string GLSUBNAME { get; set; }  
         public double DEBIT_2021 { get; set; }
         public double CREDIT_2021 { get; set; }
         public double BALANCE_2021 { get; set; }
-
-
         public double DEBIT_2022 { get; set; }
         public double CREDIT_2022 { get; set; }
         public double BALANCE_2022 { get; set; }
-
-
-
-
+        public string LAST_CREDIT { get; set; }
+        public string LAST_DEBIT { get; set; }
+        public string LAST_BALANCE { get; set; }
+        public string CURRENT_CREDIT { get; set; }
+        public string CURRENT_DEBIT { get; set; }
+        public string CURRENT_BALANCE { get; set; }
 
     }
 }
