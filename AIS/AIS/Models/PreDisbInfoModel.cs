9217abefaaa47164a68b62042818d120bb0f8b67
Mohsin Shah <engrmohsin00@gmail.com>
2024-04-16 03:14:08 -0700

    header align center and pre disable info model done

---
+++ AIS/AIS/Models/PreDisbInfoModel.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AIS.Models
+{
+    public class PreDisbInfoModel
+    {        
+       
+        public string COL1 { get; set; }
+        public string COL2 { get; set; }
+        public string COL3 { get; set; }
+        public string COL4 { get; set; }
+        public string COL5 { get; set; }
+        public string COL6 { get; set; }
+        public string COL7 { get; set; }
+        public string COL8 { get; set; }
+        public string COL9 { get; set; }
+        public string COL10 { get; set; }
+
+        public string VOL1 { get; set; }
+        public string VOL2 { get; set; }
+        public string VOL3 { get; set; }
+        public string VOL4 { get; set; }
+        public string VOL5 { get; set; }
+        public string VOL6 { get; set; }
+        public string VOL7 { get; set; }
+        public string VOL8 { get; set; }
+        public string VOL9 { get; set; }
+        public string VOL10 { get; set; }
+
+
+
+    }
+}
